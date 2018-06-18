d200d26e93cecc25fb06c89d6c2490e749c58637
xtzaferis <xtzaferis@gmail.com>
2018-06-18 09:28:41 +0200

    added body-parser and post method

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -23,8 +23,12 @@ export class PostsService {
     }
 
     addPost(title: string, content: string) {
-        const post: Post = { id: null, title: title, content: content }
-        this.posts.push(post);
-        this.postsUpdaded.next([...this.posts]);
+        const post: Post = { id: null, title: title, content: content };
+        this.http.post<{message: string}>('http://localhost:3000/api/posts', post)
+            .subscribe(response => {
+                console.log(response);
+                this.posts.push(post);
+                this.postsUpdaded.next([...this.posts]);
+            });
     }
 }
